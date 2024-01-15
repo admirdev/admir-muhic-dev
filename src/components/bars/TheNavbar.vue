cd1e106d892e0eae43748a31c8931bc4cb18e3fa
Admir <admir.muhic@outlook.com>
2024-01-15 20:16:48 +0100

    Minor fix to crate a trigger that builds the application

--- src/components/bars/TheNavbar.vue
+++ src/components/bars/TheNavbar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-white py-3 shadow-sm" id="mainNav">
     <div class="container">
-      <a class="navbar-brand fw-bold text-light-blue" href="/">admirmuhic.dev</a>
+      <router-link class="navbar-brand fw-bold text-light-blue" :to="{ path: '/' }"
+        >admirmuhic.dev</router-link
+      >
       <button
         class="navbar-toggler"
         type="button"
@@ -16,16 +18,16 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
           <li class="nav-item">
-            <a class="nav-link"><router-link :to="{ path: 'about-me' }">About me</router-link></a>
+            <router-link class="nav-link" :to="{ path: 'about-me' }">About me</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link"><router-link :to="{ path: 'projects' }">Projects</router-link></a>
+            <router-link class="nav-link" :to="{ path: 'projects' }">Projects</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link"><router-link :to="{ path: 'resume' }">Resume</router-link></a>
+            <router-link class="nav-link" :to="{ path: 'resume' }">Resume</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link"><router-link :to="{ path: 'contact' }">Contact me</router-link></a>
+            <router-link class="nav-link" :to="{ path: 'contact' }">Contact me</router-link>
           </li>
         </ul>
       </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-import { createRouter, createWebHashHistory  } from 'vue-router'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutMeView from '../views/AboutMeView.vue'
 import ProjectView from '../views/ProjectView.vue'
